ba6528cfacf3ff3238f977297d16dd738b0cf421
amir lesani <xenups@outlook.com>
2020-04-19 14:07:12 +0430

    CRUD URi added

--- app_user/urls.py
+++ app_user/urls.py
@@ -2,13 +2,13 @@
 from bottle_jwt import jwt_auth_required
 
 from ICECREAM.baseapp import BaseApp
-from app_user.controller import get_users, new_user, new_message, set_role
+from app_user.controller import get_users, create_user, get_user, delete_user
 from ICECREAM.wrappers import db_handler, pass_data, jsonify
 
 
 class USERApp(BaseApp):
     def call_router(self, core):
-        core.route('/getusers', 'GET', get_users, apply=[db_handler, jsonify, jwt_auth_required])
-        core.route('/adduser', 'POST', new_user, apply=[pass_data, db_handler, jsonify])
-        core.route('/createmessage', 'POST', new_message, apply=[pass_data, db_handler, jsonify, jwt_auth_required])
-        core.route('/set_role', 'POST', set_role, apply=[pass_data, db_handler, jsonify, jwt_auth_required])
+        core.route('/api/users/<pk>', 'GET', get_user, apply=[db_handler, jsonify, jwt_auth_required])
+        core.route('/api/users', 'GET', get_users, apply=[db_handler, jsonify, jwt_auth_required])
+        core.route('/api/user/<pk>', 'DELETE', delete_user, apply=[db_handler, jsonify, jwt_auth_required])
+        core.route('/api/user', 'POST', create_user, apply=[pass_data, db_handler, jsonify, jwt_auth_required])
